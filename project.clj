30fa627f84a5b9b1d2a88e4ae3f80815485ec4ab
Hoàng Minh Thắng <p@banphim.net>
2012-11-21 17:11:03 +0700

    set ver to 0.1.1

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject chloride "0.1.0"
+(defproject chloride "0.1.1"
   :description "Clojure/Chlorine command-line watcher/compiler"
   :url "http://github.com/myguidingstar/chloride"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.cli "0.2.2"]
-                 [chlorine "1.5.0"]
-                 [chlorine-access "1.5.0"]]
+                 [chlorine "1.5.1.1"]
+                 [chlorine-access "1.5.0.1"]]
   :main chloride.core)
