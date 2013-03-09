68ea4e75dc25265cbd6a247585db917b9b8ef12d
Hoang Minh Thang <p@banphim.net>
2013-03-10 01:02:19 +0700

    update to clojure 1.5.0

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject chloride "0.1.1-SNAPSHOT"
+(defproject chloride "0.1.2"
   :description "Clojure/Chlorine command-line watcher/compiler"
   :url "http://github.com/myguidingstar/chloride"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/tools.cli "0.2.2"]
-                 [chlorine "1.7.0-SNAPSHOT"]
+                 [chlorine-utils "1.0.0"]
                  [chlorinate "1.0.0"]]
   :main chloride.core)
