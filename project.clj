eede16aa1d4d3094f4a0ae75787ee6d300cd86ee
Hoang Minh Thang <p@banphim.net>
2013-03-11 19:25:13 +0700

    update to clojure 1.5.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/myguidingstar/chloride"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.cli "0.2.2"]
                  [chlorine-utils "1.0.0"]
                  [chlorinate "1.0.0"]]
